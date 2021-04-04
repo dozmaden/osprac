758253e3ce3e51df33f9f1ac8530775ae2db6bf8
Озмаден Дениз <dozmaden@edu.hse.ru>
2021-04-04 22:33:55 +0300

    Lab 7 is done

---
+++ lab7-task2.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int a = 0;
+
+void *mythread(void *dummy)
+{
+    pthread_t mythid;
+    mythid = pthread_self();
+    a = a + 1;
+    printf("Thread %u, Calculation result = %d\n", mythid, a);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t thid, thid2, mythid;
+    int result;
+
+    result = pthread_create( &thid, (pthread_attr_t *)NULL, mythread, NULL);
+    if (result != 0) {
+        printf ("Error on thread create, return value = %d\n", result);
+        exit(-1);
+    }
+    printf("Thread 2 created, thid = %d\n", thid);
+
+    result = pthread_create( &thid2, (pthread_attr_t *)NULL, mythread, NULL);
+    if (result != 0) {
+        printf ("Error on thread create, return value = %d\n", result);
+        exit(-1);
+    }
+    printf("Thread 3 created, thid = 	%d\n", thid2);
+
+    mythid = pthread_self();
+    a = a + 1;
+    printf("Thread %u, Calculation result = %d\n", mythid, a);
+
+    pthread_join(thid, (void **)NULL);
+    pthread_join(thid2, (void **)NULL);
+    return 0;
+}
